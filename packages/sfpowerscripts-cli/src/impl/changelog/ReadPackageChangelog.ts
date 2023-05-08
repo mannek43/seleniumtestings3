575ce7fa4c973c72fa42e8482774b299a4f462d9
bharath <mannek43@gmail.com>
2023-05-08 14:38:49 +0100

    testing sfpowerscripts

---
+++ packages/sfpowerscripts-cli/src/impl/changelog/ReadPackageChangelog.ts
@@ -0,0 +1,5 @@
+import { Changelog as PackageChangelog } from '@dxatscale/sfpowerscripts.core/lib/changelog/interfaces/GenericChangelogInterfaces';
+
+export default interface ReadPackageChangelog {
+    (changelogFilePath: string): PackageChangelog;
+}
