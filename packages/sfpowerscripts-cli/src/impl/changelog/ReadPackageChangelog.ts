c578c40bb78168d4410a30c0b84566c2a14a39e6
bharath <mannek43@gmail.com>
2023-05-17 12:31:49 +0100

    movfold to other

--- packages/sfpowerscripts-cli/src/impl/changelog/ReadPackageChangelog.ts
+++
@@ -1,5 +0,0 @@
-import { Changelog as PackageChangelog } from '@dxatscale/sfpowerscripts.core/lib/changelog/interfaces/GenericChangelogInterfaces';
-
-export default interface ReadPackageChangelog {
-    (changelogFilePath: string): PackageChangelog;
-}
--- packages/sfpowerscripts-cli/src/impl/validate/ValidateResult.ts
+++
@@ -1,8 +0,0 @@
-import { DeploymentResult, PackageInfo } from '../deploy/DeployImpl';
-import DependencyViolation from '@dxatscale/sfpowerscripts.core/lib/dependency/DependencyViolation';
-
-export default interface ValidateResult {
-    deploymentResult?: DeploymentResult;
-    dependencyViolations?: DependencyViolation[];
-    testFailures?: PackageInfo[];
-}
